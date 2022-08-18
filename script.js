2a65daf3047aa1c165093b8d7defd6b848bb7961
Felipe Bueno <felipebueno2201@gmail.com>
2022-08-18 20:34:58 -0300

    embaralhar cartas

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 let qtdCards = 0;
 const cards = [];
 while (qtdCards < 4 || qtdCards > 14 || qtdCards % 2 !== 0) {
-    qtdCards = prompt("Com quantas cartas você quer jogar?");
+    qtdCards = prompt("Com quantas cartas você quer jogar?", "Insira um número par de 4 a 14");
 }
 
 cardsIndex = document.querySelector(".cards");
 
 
-for (i = 0; i < qtdCards; i++) {
+for (i = 0; i < (qtdCards / 2); i++) {
     cards[i] = `<li class="memory-card">
-                <img class="back-face"  src="images/parrot1.gif" alt="Verso da Carta">
+                <img class="back-face"  src="images/parrot${i}.gif" alt="Verso da Carta">
                 <img class="front-face" src="images/front.png" alt="Face da Carta">
             </li>`
-                cardsIndex.innerHTML += cards[i];
+    cards[i+(qtdCards/2)] = `<li class="memory-card">
+                <img class="back-face"  src="images/parrot${i}.gif" alt="Verso da Carta">
+                <img class="front-face" src="images/front.png" alt="Face da Carta">
+            </li>`
+    
 }
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+cards.sort(comparador);
+
+for (i=0; i<qtdCards; i++) {
+    cardsIndex.innerHTML += cards[i];
+}
+
+
 
 const memoryCards = document.querySelectorAll('.memory-card');
 
 function flipCard() {
-  this.classList.add('flip');
+    this.classList.add('flip');
 }
 
 memoryCards.forEach(card => card.addEventListener('click', flipCard));
